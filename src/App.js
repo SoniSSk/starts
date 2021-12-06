8d712fe7d7a19ffbc2d1d41f57c1506ebc783d1d
SoniSSk <kuldeepsoni3099@gmail.com>
2021-12-07 02:08:29 +0530

    Sshjgbjmjhk

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header/>
       <Issue/>
       <IssueContainer/>
-        <Footer/>
+      <Footer/>
     </div>
   );
 }
--- src/component/IssueContainer.js
+++ src/component/IssueContainer.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import React from 'react';
-
 import IssueOpenedSVG from './commons/svg/IssueOpenedSVG';
 
 
